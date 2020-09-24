fecae2c85bd6a681969f57284d5a59ea9e089c09
ashleynguci <trucanhftu2@gmail.com>
2020-09-24 22:43:50 +0300

    move scret key

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import {healTheDream} from './controller';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = 3000
+
+const port = process.env.PORT || 3000;
 
 app.post('/analyze', healTheDream);
 
