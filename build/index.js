c1461174eb7f83e5a7fa23883ac9b68ebc07a046
ashleynguci <trucanhftu2@gmail.com>
2020-09-24 22:52:46 +0300

    install dotenv package

--- build/index.js
+++ build/index.js
@@ -8,14 +8,17 @@ var _bodyParser = _interopRequireDefault(require("body-parser"));
 
 var _controller = require("./controller");
 
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+_dotenv["default"].config();
+
 var app = (0, _express["default"])();
 app.use(_bodyParser["default"].json());
 app.use(_bodyParser["default"].urlencoded({
   extended: true
 }));
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.post('/analyze', _controller.healTheDream);
 app.listen(port, function () {
-  console.log(process.env);
   console.log("Example app listening at http://localhost:".concat(port));
 });
